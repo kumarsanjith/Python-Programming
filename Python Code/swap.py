ead4e68aaf74aa4b9bf0c88593d712995e5cb060
kumarsanjith <105345696+kumarsanjith@users.noreply.github.com>
2022-05-12 11:16:44 +0530

    1st commit

---
+++ Python Code/swap.py
@@ -0,0 +1,10 @@
+# Getting input from the user using input function
+a = input("The value of a: ")
+b = input("The value of b: ")
+# Switching the values
+c = a # Assigning a to a new variable
+a = b # assigning a to b
+b = c # assigining b to c
+# Displaying the values in the console
+print("a: "+a)
+print("b: "+b)
